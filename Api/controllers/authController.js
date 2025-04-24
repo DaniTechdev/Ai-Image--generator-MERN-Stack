58e6ce1cdd137ff1d3c994a6e4e9f42a93892e56
unknown <tochidan11@gmail.com>
2025-04-24 12:29:27 +0100

    done the login auth controller and geneneate the user token and attach it to the cookie

--- Api/controllers/authController.js
+++ Api/controllers/authController.js
@@ -20,13 +20,51 @@ const registerController = async (req, res, next) => {
 
     const hashedPassword = await bcrypt.hashSync(password, salt);
 
-    c;
-  } catch (error) {}
+    const newUser = new User({ ...req.body, password: hashedPassword });
+
+    const savedUser = await newUser.save();
+
+    return res.status(201).json(savedUser);
+  } catch (error) {
+    //if anything goes wrong, it will push us to error handling middleware we built
+    next(error);
+  }
 };
 
-const loginController = async (req, res, next) => {};
+const loginController = async (req, res, next) => {
+  try {
+    let user;
+    if (req.body.email) {
+      user = User.findOne({ email: req.body.email });
+    } else {
+      user = User.findOne({ username: req.body.username });
+    }
 
-const logoutController = async (req, res, next) => {};
+    if (!user) {
+      throw new CustomError("user not found", 404);
+    }
+
+    const match = await bcrypt.compare(req.body.password, user.password);
+
+    if (!match) {
+      throw new CustomError("Wrong Credentials!", 401);
+    }
+
+    const { password, ...data } = user._doc;
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_EXPIRE);
+
+    res.cookie("token", token.status(200).json(data));
+  } catch (error) {
+    next(error);
+  }
+};
+
+const logoutController = async (req, res, next) => {
+  //logout can only happen when we remove the coockies from the user browser
+  try {
+  } catch (error) {}
+};
 
 const refetchController = async (req, res, next) => {};
 
